2ef6ad1db91c5704e84de3ef076b28ebbda53847
cristobalJR <80638478+cristobalJR@users.noreply.github.com>
2026-06-16 01:52:24 +0200

    fin comentario profes

--- SAVBC/src/neartrainnetwork/CommuterTicketDispenser.java
+++ SAVBC/src/neartrainnetwork/CommuterTicketDispenser.java
@@ -1,7 +1,13 @@
 package neartrainnetwork;
 
 /**
+ * ----------------------------------------------------------------------------------------------
+ * Comentario para los profesores: No he podido corregir el problema con los caracteres del catalán. 
+ * Supongo que tiene que ver con el uso de Unicode, pero no he podido solventarlo.
+ * -----------------------------------------------------------------------------------------------
+ * 
  * Clase principal: arranca la aplicación.
+ * 
  */
 public class CommuterTicketDispenser {
 
